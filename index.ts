e743ae94e808aecc6e7a25773cf6fd186be80284
Brendan Murty <b@murty.io>
2021-07-02 21:30:27 +1000

    Improve code style

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import "https://deno.land/x/dotenv/load.ts";
 
 const EnvToken: string = Deno.env.get("UP_PERSONAL_ACCESS_TOKEN") || "";
+
 const ApiUrlAccounts: string = 'https://api.up.com.au/api/v1/accounts';
 
 await fetch(ApiUrlAccounts, {
@@ -8,5 +9,12 @@ await fetch(ApiUrlAccounts, {
     headers: {
       'Authorization': 'Bearer ' + EnvToken 
     }
-  }).then(response => response.json())
-  .then(data => { for (const index in data.data) { let AccountInfo = data.data[index].attributes; console.log(" - " + AccountInfo.displayName + ": " + AccountInfo.balance.currencyCode + " " + AccountInfo.balance.value); } });
+  })
+.then(response => response.json())
+.then(data => {
+  for (const index in data.data) {
+    let AccountInfo = data.data[index].attributes;  
+
+    console.log(" - " + AccountInfo.displayName + ": " + AccountInfo.balance.currencyCode + " " + AccountInfo.balance.value);
+  }
+});
