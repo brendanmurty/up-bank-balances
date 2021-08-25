fde587950c704ecf920677a5df31c6d3080dd3b5
Brendan Murty <b@murty.io>
2021-08-25 23:53:24 +1000

    Add recent transactions list and section headings

--- index.ts
+++ index.ts
@@ -7,10 +7,12 @@ if (EnvToken == '') {
   Deno.exit(1);
 }
 
-const ApiUrlAccounts: string = 'https://api.up.com.au/api/v1/accounts';
+const ApiUrl: string = 'https://api.up.com.au/api/v1';
+
+console.log("\nAccounts:\n");
 
 await fetch(
-  ApiUrlAccounts,
+  ApiUrl + '/accounts',
   {
     method: 'GET',
     headers: {
@@ -21,8 +23,32 @@ await fetch(
 .then(response => response.json())
 .then(data => {
   for (const index in data.data) {
+    let AccountId = data.data[index].id;  
     let AccountInfo = data.data[index].attributes;  
 
     console.log(" - " + AccountInfo.displayName.trim() + ": $" + AccountInfo.balance.value);
   }
 });
+
+console.log("\nTransactions:\n");
+
+await fetch(
+  ApiUrl + '/transactions?page[size]=5',
+  {
+    method: 'GET',
+    headers: {
+      'Authorization': 'Bearer ' + EnvToken 
+    }
+  }
+)
+.then(response => response.json())
+.then(data => {
+  for (const index in data.data) {
+    let TransactionInfo = data.data[index].attributes
+    let TransactionAmount = ("$" + TransactionInfo.amount.value).replace("$-", "-$");
+
+    console.log(" - " + TransactionInfo.description.trim() + ": " + TransactionAmount);
+  }
+})
+
+console.log("\n");
