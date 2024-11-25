c16734718ae17ca2e2db4a4d18b8d8eed9d31664
Brendan Murty <brendan@murty.au>
2024-11-26 09:15:50 +1100

    Set account request limit

--- src/up-bank-account-summary.ts
+++ src/up-bank-account-summary.ts
@@ -8,7 +8,7 @@ export async function UpBankAccountSummary(
 
   // Get a list of all open accounts
   const ApiResponse = await fetch(
-    ApiUrl + "/accounts",
+    ApiUrl + "/accounts?page[size]=30",
     {
       method: "GET",
       headers: {
